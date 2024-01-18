776876c17262708482f1f824d3827eecd20b9309
Ananya K <akidambi.26@presentationhs.org>
2024-01-17 18:30:42 -0800

    Added setpoint control
    Still a work in progress

--- Prototype2024/src/main/java/frc/robot/Robot.java
+++ Prototype2024/src/main/java/frc/robot/Robot.java
@@ -1,16 +1,31 @@
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.MathUtil;
+import frc.robot.ExampleSmartMotorController.PIDMode;
 
 
 public class Robot extends TimedRobot
 {
+  private final static double                       kDt           = 0.020;  // 20 msec per RoboRIO loop
+
+  private final static double                       kv            = 8.0;    // Max velocity - RPS
+  private final static double                       ka            = 16.0;   // Max acceleration - RPS^2
+
+  private final static double                       goal_1        = 0.0;    // Goal 1 position
+  private final static double                       goal_2        = 0.5;    // Goal 2 position
+
   private final static XboxController               m_controller  = new XboxController(1);
   private final static ExampleSmartMotorController  m_motor1       = new ExampleSmartMotorController(5);
   private final static ExampleSmartMotorController  m_motor2       = new ExampleSmartMotorController(6);
+  
+  private final static TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(kv, ka);
+  private TrapezoidProfile.State                    m_goal        = new TrapezoidProfile.State( );
+  private TrapezoidProfile.State                    m_setpoint    = new TrapezoidProfile.State( );
+
 
   @Override
   public void robotInit( )
@@ -18,6 +33,7 @@ public class Robot extends TimedRobot
     double def = 0.5;
     SmartDashboard.putNumber("CustomSpeed", def);
     SmartDashboard.putNumber("MotorVoltage", 0);
+    SmartDashboard.putString("tester", "nope");
   }
 
   @Override
@@ -36,9 +52,25 @@ public class Robot extends TimedRobot
       double stickDeadband = MathUtil.applyDeadband(stickValue, 0.15);
       motorOutput = stickDeadband;
     }
-    m_motor1.set(motorOutput); 
-    m_motor2.set(-1*motorOutput); 
-    SmartDashboard.putNumber("MotorVoltage", motorOutput); 
+    else if (m_controller.getRightBumper()){
+      if (m_controller.getXButton( ) && m_controller.getRightBumper()){
+        m_goal = new TrapezoidProfile.State(goal_1, 0);
+        SmartDashboard.putString("tester", "totally");
+      }
+      else if (m_controller.getBButton( ) && m_controller.getRightBumper())
+        m_goal = new TrapezoidProfile.State(goal_2, 0);
+      SmartDashboard.putNumber("0-goal", m_goal.position);
+      TrapezoidProfile profile = new TrapezoidProfile(m_constraints, m_goal, m_setpoint);
+      m_setpoint = profile.calculate(kDt);
+      m_motor1.setSetpoint(PIDMode.kPosition, m_setpoint.position, 0.0);
+      SmartDashboard.putString("tester", "out");
+      SmartDashboard.putNumber("1-setpoint", m_setpoint.position);
+    }
+
+    //#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//#//
+    // m_motor1.set(motorOutput); 
+    // m_motor2.set(-1*motorOutput); 
+    // SmartDashboard.putNumber("MotorVoltage", motorOutput); 
   }
 
   @Override

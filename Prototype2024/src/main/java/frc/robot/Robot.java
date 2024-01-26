f218ed7f5208f344e805021591376d9ed3b41587
Ananya K <akidambi.26@presentationhs.org>
2024-01-25 19:14:16 -0800

    Fixed setpoint conversion code

--- Prototype2024/src/main/java/frc/robot/Robot.java
+++ Prototype2024/src/main/java/frc/robot/Robot.java
@@ -1,8 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,8 +21,9 @@ public class Robot extends TimedRobot
   private final static double                       goal_2        = 0.5;    // Goal 2 position
 
   private final static XboxController               m_controller  = new XboxController(1);
-  private final static ExampleSmartMotorController  m_motor1      = new ExampleSmartMotorController(5);
-  private final static ExampleSmartMotorController  m_motor2      = new ExampleSmartMotorController(6);
+  private final static ExampleSmartMotorController  m_motor       = new ExampleSmartMotorController(5);
+  // Note: These gains are fake, and will have to be tuned for your robot.
+  // private final SimpleMotorFeedforward              m_feedforward = new SimpleMotorFeedforward(ks, kf);
 
   private final static TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(kv, ka);
   private TrapezoidProfile.State                    m_goal        = new TrapezoidProfile.State( );
@@ -38,77 +41,49 @@ public class Robot extends TimedRobot
   @Override
   public void teleopPeriodic( )
   {
-
-    //custom speed + joystick
     double motorOutput = 0.0;
-    if ((m_controller.getAButtonPressed( )))
-    {
-      motorOutput = (SmartDashboard.getNumber("CustomSpeed", motorOutput));
-      m_motor1.set(motorOutput);
-      m_motor2.set(-1 * motorOutput);
-      SmartDashboard.putNumber("MotorVoltage", motorOutput);
-    }
-    if ((m_controller.getBButtonPressed( )))
-    {
-      motorOutput = (SmartDashboard.getNumber("CustomSpeed", motorOutput));
-      m_motor1.set(-1 * motorOutput);
-      m_motor2.set(motorOutput);
-      SmartDashboard.putNumber("MotorVoltage", motorOutput * -1);
-    }
-    if ((m_controller.getRightBumper( )))
-    {
-      motorOutput = (0);
-      m_motor1.set(motorOutput);
-      m_motor2.set(motorOutput);
-      SmartDashboard.putNumber("MotorVoltage", motorOutput);
-    }
     if (m_controller.getLeftBumper( ))
     {
-      double stickValue = m_controller.getRightY( );
-      double stickDeadband = MathUtil.applyDeadband(stickValue, 0.15);
-      motorOutput = stickDeadband;
-      m_motor1.set(motorOutput);
-      m_motor2.set(-1 * motorOutput);
-      SmartDashboard.putNumber("MotorVoltage", motorOutput);
+      if ((m_controller.getAButtonPressed( )))
+      {
+        motorOutput = (SmartDashboard.getNumber("CustomSpeed", motorOutput));
+        m_motor.set(motorOutput);
+        SmartDashboard.putNumber("MotorVoltage", motorOutput);
+      }
+      if ((m_controller.getBButtonPressed( )))
+      {
+        motorOutput = (SmartDashboard.getNumber("CustomSpeed", motorOutput));
+        m_motor.set(-1 * motorOutput);
+        SmartDashboard.putNumber("MotorVoltage", motorOutput * -1);
+      }
     }
-
-    //setpoint code 
-    if (m_controller.getXButton( ))
+    if (m_controller.getRightBumper( ))
     {
-      m_goal = new TrapezoidProfile.State(goal_1, 0);
-      DataLogManager.log("goal: " + m_goal);
+      if (m_controller.getXButton( ))
+        m_goal = new TrapezoidProfile.State(goal_1, 0);
+      else if (m_controller.getYButton( ))
+        m_goal = new TrapezoidProfile.State(goal_2, 0);
       SmartDashboard.putNumber("0-goal", m_goal.position);
-      DataLogManager.log("goal position: " + m_goal.position);
-
-      TrapezoidProfile profile = new TrapezoidProfile(m_constraints, m_goal);
-      m_setpoint = profile.calculate(kDt);
-
-      m_motor1.setSetpoint(PIDMode.kPosition, m_setpoint.position, 0.0);
-      SmartDashboard.putNumber("1-setpoint", m_setpoint.position);
-
-    }
-    else if (m_controller.getYButton( ))
-    {
-      m_goal = new TrapezoidProfile.State(goal_2, 0);
-      DataLogManager.log("goal: " + m_goal);
-      SmartDashboard.putNumber("0-goal", m_goal.position);
-      DataLogManager.log("goal position: " + m_goal.position);
-
-      TrapezoidProfile profile = new TrapezoidProfile(m_constraints, m_goal);
-      m_setpoint = profile.calculate(kDt);
-
-      m_motor1.setSetpoint(PIDMode.kPosition, m_setpoint.position, 0.0);
-      SmartDashboard.putNumber("1-setpoint", m_setpoint.position);
-
     }
 
+    // Create a motion profile with the given maximum velocity and maximum
+    // acceleration constraints for the next setpoint, the desired goal, and the
+    // current setpoint.
+    TrapezoidProfile profile = new TrapezoidProfile(m_constraints, m_goal, m_setpoint);
+
+    // Retrieve the profiled setpoint for the next timestep. This setpoint moves
+    // toward the goal while obeying the constraints.
+    m_setpoint = profile.calculate(kDt);
+
+    // Send setpoint to offboard controller PID
+    m_motor.setSetpoint(PIDMode.kPosition, m_setpoint.position, 0.0);
+    SmartDashboard.putNumber("1-setpoint", m_setpoint.position);
   }
 
   @Override
   public void robotPeriodic( )
   {
-    m_motor1.periodic( );
-    m_motor2.periodic( );
+    m_motor.periodic( );
   }
 
 }

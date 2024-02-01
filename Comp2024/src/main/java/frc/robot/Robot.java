a5573feea34b6d794ad946613627fbb80384ff9c
grace-mills <gmills.25@presentationhs.org>
2024-01-31 17:14:14 -0800

    limelight simulation in comp2024

--- Comp2024/src/main/java/frc/robot/Robot.java
+++ Comp2024/src/main/java/frc/robot/Robot.java
@@ -24,7 +24,7 @@ import frc.robot.lib.util.LimelightHelpers;
  */
 public class Robot extends TimedRobot
 {
-  private final boolean      m_useLimelight  = false;
+  private final boolean      m_useLimelight  = true;
   public boolean             m_isComp        = false;
 
   private RobotContainer     m_robotContainer;

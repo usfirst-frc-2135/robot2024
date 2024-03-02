0339439d8a704f00b62bf8376a5e17dec0b3a9f9
Ananya K <akidambi.26@presentationhs.org>
2024-03-02 14:55:35 -0800

    Cleaned up formatting in Intake commands

--- Comp2024/src/main/java/frc/robot/commands/IntakeActionAcquire.java
+++ Comp2024/src/main/java/frc/robot/commands/IntakeActionAcquire.java
@@ -31,14 +31,11 @@ public class IntakeActionAcquire extends SequentialCommandGroup
         new PrintCommand(getName() + ": Start rollers & Deploy intake rotary"),
         new IntakeRun(intake, INConsts.RollerMode.ACQUIRE, INConsts.kRotaryAngleDeployed),
 
-
         new PrintCommand(getName() + ": Wait for note"),
         new WaitUntilCommand(intake::isNoteDetected),
 
         new PrintCommand(getName() + ": Stop rollers & Retract intake rotary"),
         new IntakeRun(intake, INConsts.RollerMode.STOP, INConsts.kRotaryAngleRetracted)
-
- 
         // @formatter:on
     );
   }
--- Comp2024/src/main/java/frc/robot/commands/IntakeActionRetract.java
+++ Comp2024/src/main/java/frc/robot/commands/IntakeActionRetract.java
@@ -33,8 +33,8 @@ public class IntakeActionRetract extends SequentialCommandGroup
       
         new PrintCommand(getName() + ": Stop rollers & Retract intake rotary"),
         new IntakeRun(intake, INConsts.RollerMode.STOP, INConsts.kRotaryAngleRetracted)
+        //@formatter:on
     );
-
 
   }
 

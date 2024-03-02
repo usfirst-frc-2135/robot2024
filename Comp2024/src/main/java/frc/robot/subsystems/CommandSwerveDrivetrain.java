c8111e5e87b068af478dbfeb03da398b56acab07
Jeff Mullins <jeff.l.mullins@gmail.com>
2024-03-01 19:15:02 -0800

    Added Smartdashboard widgets to allow teleporting the robot anywhere on the field during simulation

--- Comp2024/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ Comp2024/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -27,6 +27,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants.VIConsts;
 import frc.robot.generated.TunerConstants;
 import frc.robot.lib.util.LimelightHelpers;
 

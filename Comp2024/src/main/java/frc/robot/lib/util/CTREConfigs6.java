e72a7336aec75db40dd463520034174f0e406a00
Jeff Mullins <jeff.l.mullins@gmail.com>
2024-03-24 19:41:12 -0700

    Reverting intake rotary kP until the reason can be confirmed

--- Comp2024/src/main/java/frc/robot/lib/util/CTREConfigs6.java
+++ Comp2024/src/main/java/frc/robot/lib/util/CTREConfigs6.java
@@ -65,7 +65,7 @@ public final class CTREConfigs6
     // Slot settings
     inRotaryConfig.Slot0.kS = 0.0;                                // Voltage or duty cylce to overcome static friction
     inRotaryConfig.Slot0.kV = 0.1129;                             // Voltage or duty cycle per requested RPS (velocity modes)
-    inRotaryConfig.Slot0.kP = 0.0625;                                // Voltage or duty cycle per velocity error (velocity modes)
+    inRotaryConfig.Slot0.kP = 0.2;                                // Voltage or duty cycle per velocity error (velocity modes)
     inRotaryConfig.Slot0.kI = 0.0;                                // Voltage or duty cycle per accumulated error
     inRotaryConfig.Slot0.kD = 0.0;                                // Voltage or duty cycle per unit of acceleration error (velocity modes)
 

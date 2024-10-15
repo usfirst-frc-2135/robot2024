9356c3ba43d1c8e9a740689a6d1fd27e571bfc19
Jeff Mullins <jeff.l.mullins@gmail.com>
2024-10-15 14:25:15 -0700

    Updated Prototype2024 to be a better example for new members

--- Prototype2024/src/main/java/frc/robot/Robot.java
+++ Prototype2024/src/main/java/frc/robot/Robot.java
@@ -1,43 +1,48 @@
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.MathUtil;
+import frc.robot.ExampleSmartMotorController.PIDMode;
 
 public class Robot extends TimedRobot
 {
-  private final static XboxController              m_controller = new XboxController(1);
-  private final static ExampleSmartMotorController m_motor1     = new ExampleSmartMotorController(5);
-  private final static ExampleSmartMotorController m_motor2     = new ExampleSmartMotorController(6);
+  private static enum ControlMode
+  {
+    kStopped, kFixedSpeed, kJoystickControl, kClosedLoop
+  }
+
+  private final static XboxController               m_controller    = new XboxController(0);
+  private final static ExampleSmartMotorController  m_motor1        = new ExampleSmartMotorController(5);
+  private final static ExampleSmartMotorController  m_motor2        = new ExampleSmartMotorController(6);
+
+  private final static double                       kv              = 1.0;    // Max velocity - RPS
+  private final static double                       ka              = 2.0;    // Max acceleration - RPS^2
+
+  private final static TrapezoidProfile.Constraints m_constraints   = new TrapezoidProfile.Constraints(kv, ka);
+
+  private final static double                       m_goal1         = 0.5;    // Goal 1 position
+  private final static double                       m_goal2         = -1.0;   // Goal 2 position
+
+  private ControlMode                               m_controlMode   = ControlMode.kStopped;
+  private double                                    m_fixedSpeed    = 0.3;
+  private double                                    m_percentOutput = 0.0;
+
+  private Timer                                     m_timer         = new Timer( );
+  private TrapezoidProfile                          m_profile       = new TrapezoidProfile(m_constraints);
+  private TrapezoidProfile.State                    m_goal          = new TrapezoidProfile.State( );
+  private TrapezoidProfile.State                    m_setpoint      = new TrapezoidProfile.State( );
 
   @Override
   public void robotInit( )
   {
-    double def = 0.5;
-    SmartDashboard.putNumber("CustomSpeed", def);
-    SmartDashboard.putNumber("MotorVoltage", 0);
-  }
-
-  @Override
-  public void teleopPeriodic( )
-  {
-    double motorOutput = 0.0;
-    if (m_controller.getBButton( ))
-      motorOutput = 0.5;
-    else if (m_controller.getYButton( ))
-      motorOutput = (SmartDashboard.getNumber("CustomSpeed", motorOutput));
-    else if (m_controller.getAButton( ))
-      motorOutput = 1;
-    else if (m_controller.getLeftBumper( ))
-    {
-      double stickValue = m_controller.getRightY( );
-      double stickDeadband = MathUtil.applyDeadband(stickValue, 0.15);
-      motorOutput = stickDeadband;
-    }
-    m_motor1.set(motorOutput);
-    m_motor2.set(-1 * motorOutput);
-    SmartDashboard.putNumber("MotorVoltage", motorOutput);
+    DataLogManager.start( );
+    SmartDashboard.putNumber("FixedSpeed", m_fixedSpeed);
+    DataLogManager.log(String.format("***** Prototype2024 starting up *****"));
   }
 
   @Override
@@ -45,6 +50,100 @@ public class Robot extends TimedRobot
   {
     m_motor1.periodic( );
     m_motor2.periodic( );
+  }
+
+  @Override
+  public void disabledInit( )
+  {
+    m_controlMode = ControlMode.kStopped;
+    m_goal.position = 0.0;
+    m_goal.velocity = 0.0;
+    m_setpoint.position = 0.0;
+    m_setpoint.velocity = 0.0;
+    m_motor1.set(0.0);
+    m_motor2.set(0.0);
+    m_motor1.resetEncoder( );
+    m_motor2.resetEncoder( );
+  }
+
+  @Override
+  public void teleopPeriodic( )
+  {
+    // Detect mode changes
+
+    if (m_controller.getAButtonPressed( ) || m_controller.getBButtonPressed( ))
+    {
+      m_controlMode = ControlMode.kFixedSpeed;
+      Boolean aButton = m_controller.getAButton( );
+      m_fixedSpeed = SmartDashboard.getNumber("FixedSpeed", m_fixedSpeed);
+      DataLogManager.log(String.format("%s button pressed - percent output: %.2f", (aButton) ? "A" : "B", m_fixedSpeed));
+      m_percentOutput = (aButton) ? m_fixedSpeed : -m_fixedSpeed;
+    }
+    else if (m_controller.getRightBumperPressed( ))
+    {
+      m_controlMode = ControlMode.kStopped;
+      DataLogManager.log(String.format("Left bumper pressed - STOP!"));
+      m_percentOutput = 0.0;
+    }
+    else if (m_controller.getLeftBumperPressed( ))
+    {
+      m_controlMode = ControlMode.kJoystickControl;
+      DataLogManager.log(String.format("Right bumper pressed - joystick control"));
+
+    }
+    else if (m_controller.getXButtonPressed( ) || m_controller.getYButtonPressed( ))
+    {
+      m_controlMode = ControlMode.kClosedLoop;
+      Boolean xButton = m_controller.getXButton( );
+      DataLogManager.log(String.format("%s button pressed", (xButton ? "X" : "Y")));
+      m_timer.restart( );
+      m_motor1.resetEncoder( );
+      m_motor2.resetEncoder( );
+      m_setpoint = new TrapezoidProfile.State( );
+      m_goal = new TrapezoidProfile.State(((xButton) ? m_goal1 : m_goal2), 0);
+      DataLogManager.log(String.format("Start: goal: %.2f setpoint: %.2f Position: %.3f", m_goal.position, m_setpoint.position,
+          m_motor1.getEncoderPosition( )));
+    }
+
+    // Run the correct mode each loop
+
+    switch (m_controlMode)
+    {
+      case kJoystickControl :
+        m_percentOutput = MathUtil.applyDeadband(m_controller.getLeftY( ), 0.15);
+      default :
+      case kStopped :
+      case kFixedSpeed :
+        m_motor1.set(m_percentOutput);
+        m_motor2.set(-m_percentOutput);
+        break;
+
+      case kClosedLoop :
+      {
+        double position = m_motor1.getEncoderPosition( );
+        DataLogManager.log(
+            String.format("Loop:  goal: %.2f setpoint: %.2f Position: %.3f", m_goal.position, m_setpoint.position, position));
+        if (m_timer.hasElapsed(4.0) || Math.abs(position - m_goal.position) < 0.05)
+        {
+          DataLogManager.log(String.format("Return to open loop - %s", (m_timer.hasElapsed(4.0) ? "timed out" : "at goal")));
+          m_controlMode = ControlMode.kStopped;
+          m_percentOutput = 0.0;
+        }
+        else
+        {
+          DataLogManager.log(
+              String.format("Loop:  goal: %.2f setpoint: %.2f Position: %.3f", m_goal.position, m_setpoint.position, position));
+          m_setpoint = m_profile.calculate(m_timer.get( ), m_setpoint, m_goal);
+          m_motor1.setSetpoint(PIDMode.kPosition, m_setpoint.position, 0.0);
+          m_motor2.set(0.0);
+        }
+      }
+        break;
+    }
+
+    SmartDashboard.putNumber("LOOP-goal", m_goal.position);
+    SmartDashboard.putNumber("LOOP-m_motor1", m_motor1.get( ));
+    SmartDashboard.putNumber("LOOP-m_motor2", m_motor2.get( ));
   }
 
 }
